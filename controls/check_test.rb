db0bcc4e149c676b621f975ac89b086ccf6d3121
Stuart Paterson <spaterson@chef.io>
2020-06-08 15:32:44 +0100

    Rename the inspec test attribute for clarity.
    Signed-off-by: Stuart Paterson <spaterson@chef.io>

--- controls/check_test.rb
+++ controls/check_test.rb
@@ -8,28 +8,26 @@ control 'core-plans-check' do
   impact 1.0
   title 'Ensure check works'
   desc '
-  We confirm that the check executable being present in the expected location.
-
-  We also run the following to ensure checkmk is correctly packaged:
+  We run the following to ensure checkmk is correctly packaged:
     $ ls -al <pkg>/lib/libcheck.so
     $ ls -al <pkg>/bin/checkmk
   '
-  expect_pkg_ident = command("#{hab_path} pkg path #{plan_ident}")
-  describe expect_pkg_ident do
+  check_pkg_ident = command("#{hab_path} pkg path #{plan_ident}")
+  describe check_pkg_ident do
     its('exit_status') { should eq 0 }
     its('stdout') { should_not be_empty }
   end
-  expect_pkg_ident = expect_pkg_ident.stdout.strip
+  check_pkg_ident = check_pkg_ident.stdout.strip
 
-  describe command("ls -al #{expect_pkg_ident}/lib/libcheck.so") do
+  describe command("ls -al #{check_pkg_ident}/lib/libcheck.so") do
     its('stdout') { should_not be_empty }
-    its('stdout') { should match /#{expect_pkg_ident}/}
+    its('stdout') { should match /#{check_pkg_ident}/}
     its('exit_status') { should eq 0 }
   end
 
-  describe command("ls -al #{expect_pkg_ident}/bin/checkmk") do
+  describe command("ls -al #{check_pkg_ident}/bin/checkmk") do
     its('stdout') { should_not be_empty }
-    its('stdout') { should match /#{expect_pkg_ident}/}
+    its('stdout') { should match /#{check_pkg_ident}/}
     its('exit_status') { should eq 0 }
   end
 end
